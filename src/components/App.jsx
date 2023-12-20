484abec94e4a1122d579af9089861f3f5adeaa36
pavloit <dba.devop@gmail.com>
2023-12-20 15:54:59 +0200

    adding contacts to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import ContactForm from './contactform/ContactForm';
 import ContactList from './contactlist/ContactList';
 import Filter from './filter/Filter';
 import styled from 'styled-components';
+import { nanoid } from 'nanoid';
 
 const CenteredContainer = styled.div`
   display: flex;
@@ -14,12 +15,31 @@ const CenteredContainer = styled.div`
 
 class App extends Component {
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [],
     filter: '',
   };
+
+componentDidMount = () => {
+  const localContacts = localStorage.getItem('contacts');
+  if (!localContacts) {
+    this.setState({
+      contacts: [      
+        { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+        { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+        { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+        { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    })
+    return
+  }
+  this.setState({ contacts: JSON.parse(localContacts) })
+}
+  
+  componentDidUpdate = (_, prevState) => {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   deleteContact = (id) => {
     this.setState(prevState => ({
